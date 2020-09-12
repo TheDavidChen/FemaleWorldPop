2f4121be622a334a4fbff86c703ecb2cf4f73f96
TheDavidChen <dzc89@psu.edu>
2020-09-12 11:35:05 -0400

    Added comments and fixed column names

--- 3_joinFiles.R
+++ 3_joinFiles.R
@@ -1,4 +1,17 @@
+#############################################################################
+# Date: August 19th, 2020
+# Author: David Chen 
 
+# Purpose: 
+#   Combine all the previously downloaded population data into one dataframe. 
+
+# Input:
+#   Specify the folder with the previous output and label the output name.
+
+# Output:
+#   All the individual years combined into one dataframe. 
+
+#############################################################################
 
 # Join all the data from our output dataset
 folder <- './Output'
@@ -6,6 +19,7 @@ filenames <- list.files(folder)
 
 # Join all the datasets together
 all_files <- Reduce(rbind, lapply(paste0(folder, "/" ,filenames), readRDS))
+names(all_files) <- c("country", "admin_level_name", "fpop_sum", "year", "age")
 
 # Save the output, manually specify the name. 
 saveRDS(all_files, "./Female1549_00_20.RDS")
